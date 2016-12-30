9ece4739d36e6ec3a080969d9211b1639b13a89d
Mayank Chandak <chandakmayank@gmail.com>
2016-12-30 18:39:08 +0530

    one character now moves on the pune map

---
+++ constants.js
@@ -0,0 +1,31 @@
+//------------
+//System Values
+//------------
+var STAGE_WIDTH = 1440,
+	STAGE_HEIGHT = 900,
+	TIME_PER_FRAME = 33, //this equates to 30 fps
+	GAME_FONTS = "bold 20px sans-serif";
+
+var PATH_CHAR = "img/spritesheet.png";
+var PATH_MAP = "img/pune map.png";
+
+
+var CHAR_WIDTH = 72,
+	CHAR_HEIGHT = 96,
+	CHAR_START_X = 200,
+	CHAR_START_Y = 200,
+	CHAR_SPEED = 5,
+	IMAGE_START_X = 0,
+	IMAGE_START_NORTH_Y = 0,
+	IMAGE_START_EAST_Y = 96,
+	IMAGE_START_SOUTH_Y = 192,
+	IMAGE_START_WEST_Y = 288,	
+	SPRITE_WIDTH = 216;
+
+var TEXT_PRELOADING = "Loading ...", 
+	TEXT_PRELOADING_X = 200, 
+	TEXT_PRELOADING_Y = 200;
+	
+	
+	
+	
